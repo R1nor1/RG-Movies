84d92272dbb3e09276a6eda3ae119624a8132c7a
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2021-12-15 19:15:36 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function openPage() {
+    var x = document.getElementById("search").value;
+
+    if (x === "Seriale", "seriale") {
+        window.open("../Project/Seriale.html");
+    }
+
+    if (x === "Home", "home") {
+        window.open("http://127.0.0.1:5500/index.html");
+    }
+
+    if (x === "Anime", "anime") {
+        window.open("http://127.0.0.1:5500/Anime.html");
+    }
+
+}
